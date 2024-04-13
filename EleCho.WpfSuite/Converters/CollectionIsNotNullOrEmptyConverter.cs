715849b4bfeb692dc29a33d3583bdda174e8a318
SlimeNull <slimenull@outlook.com>
2024-04-13 12:49:06 +0800

    feat: 添加集合与字符串相关转换器

---
+++ EleCho.WpfSuite/Converters/CollectionIsNotNullOrEmptyConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
+
+namespace EleCho.WpfSuite
+{
+    public class CollectionIsNotNullOrEmptyConverter : SingletonValueConverterBase<CollectionIsNotNullOrEmptyConverter>
+    {
+        static readonly Type _typeGenericCollection = typeof(ICollection<>);
+        static readonly PropertyInfo _genericCollectionCountProperty = _typeGenericCollection.GetProperty(nameof(ICollection<object>.Count))!;
+
+        public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return false;
+
+            if (value is ICollection nonGenericCollection)
+            {
+                return nonGenericCollection.Count != 0;
+            }
+            else
+            {
+                var valueType = value.GetType();
+                if (valueType.IsGenericType && valueType.GetGenericTypeDefinition() == _typeGenericCollection)
+                {
+                    return (int)(_genericCollectionCountProperty.GetValue(value)!) != 0;
+                }
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
