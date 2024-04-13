21672680165749bbdd84435172c0801a596c70ed
SlimeNull <slimenull@outlook.com>
2024-04-13 12:48:29 +0800

    fix: 修复 ValueConverterGroup 使用 bug

--- EleCho.WpfSuite/Converters/ValueConverterGroup.cs
+++ EleCho.WpfSuite/Converters/ValueConverterGroup.cs
@@ -4,14 +4,16 @@ using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace EleCho.WpfSuite
 {
+    [ContentProperty(nameof(Converters))]
     public class ValueConverterGroup : ValueConverterBase<ValueConverterGroup>
     {
-        public IEnumerable<IValueConverter>? Converters
+        public List<IValueConverter> Converters
         {
-            get { return (IEnumerable<IValueConverter>)GetValue(ConvertersProperty); }
+            get { return (List<IValueConverter>)GetValue(ConvertersProperty); }
             set { SetValue(ConvertersProperty, value); }
         }
 
@@ -20,7 +22,7 @@ namespace EleCho.WpfSuite
             if (Converters is null)
                 return value;
 
-            return Converters.Aggregate(value, (current, converter) => converter.Convert(value, targetType, parameter, culture));
+            return Converters.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
         }
 
         public override object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
@@ -28,10 +30,10 @@ namespace EleCho.WpfSuite
             if (Converters is null)
                 return value;
 
-            return Converters.Reverse().Aggregate(value, (current, converter) => converter.Convert(value, targetType, parameter, culture));
+            return Converters.Reverse<IValueConverter>().Aggregate(value, (current, converter) => converter.Convert(value, targetType, parameter, culture));
         }
 
         public static readonly DependencyProperty ConvertersProperty =
-            DependencyProperty.Register(nameof(Converters), typeof(IEnumerable<IValueConverter>), typeof(ValueConverterGroup), new PropertyMetadata(null));
+            DependencyProperty.Register(nameof(Converters), typeof(List<IValueConverter>), typeof(ValueConverterGroup), new PropertyMetadata(new List<IValueConverter>()));
     }
 }
