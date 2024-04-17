217b66fb55578caf66152133e738af0f0d43c49b
SlimeNull <slimenull@outlook.com>
2024-04-17 16:09:49 +0800

    feat: 加入 "设计风格" 支持

---
+++ EleCho.WpfSuite.SimpleDesign/SimpleDesignResources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace EleCho.WpfSuite.SimpleDesign
+{
+    public class SimpleDesignResources : ResourceDictionary
+    {
+        public SimpleDesignResources()
+        {
+            MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("pack://application:,,,/EleCho.WpfSuite.SimpleDesign;component/SimpleDesignResources.xaml") });
+        }
+    }
+}
