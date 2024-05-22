c4f7e9a1f061fe5aa95694a9242b010291bdf972
SlimeNull <slimenull@outlook.com>
2024-05-22 19:26:27 +0800

    feat: 添加了 GroupBox

---
+++ EleCho.WpfSuite/Controls/GroupBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace EleCho.WpfSuite
+{
+    public class GroupBox : System.Windows.Controls.GroupBox
+    {
+        static GroupBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GroupBox), new FrameworkPropertyMetadata(typeof(GroupBox)));
+        }
+
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            Border.CornerRadiusProperty.AddOwner(typeof(GroupBox), new FrameworkPropertyMetadata(new CornerRadius(3)));
+    }
+}
