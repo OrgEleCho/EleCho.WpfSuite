d4b7ddbc920bbb96610f0d5c860bb694ed1e7934
SlimeNull <slimenull@outlook.com>
2024-05-22 19:15:06 +0800

    feat: 添加 TabControl 和 TabItem

---
+++ EleCho.WpfSuite/Controls/TabControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace EleCho.WpfSuite
+{
+    public class TabControl : System.Windows.Controls.TabControl
+    {
+        static TabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TabControl), new FrameworkPropertyMetadata(typeof(TabControl)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            Border.CornerRadiusProperty.AddOwner(typeof(TabControl));
+    }
+}
