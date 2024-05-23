6d0671f5d5217b2fcd7e65ec05e409ff5ef0e474
SlimeNull <slimenull@outlook.com>
2024-05-23 19:27:27 +0800

    feat: 添加 ClampNumberConverter

---
+++ EleCho.WpfSuite/Converters/ClampNumberConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace EleCho.WpfSuite
+{
+    public class ClampNumberConverter : ValueConverterBase<DivideNumberConverter>
+    {
+        public double Minimum
+        {
+            get { return (double)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register(nameof(Minimum), typeof(double), typeof(ClampNumberConverter), new PropertyMetadata(0.0));
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register(nameof(Maximum), typeof(double), typeof(ClampNumberConverter), new PropertyMetadata(1.0));
+
+        public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            try
+            {
+                double current = System.Convert.ToDouble(value);
+                if (current < Minimum)
+                    current = Minimum;
+                if (current > Maximum)
+                    current = Maximum;
+
+                var result = current;
+                if (typeof(IConvertible).IsAssignableFrom(targetType))
+                {
+                    return System.Convert.ChangeType(result, targetType);
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            catch
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+    }
+}
