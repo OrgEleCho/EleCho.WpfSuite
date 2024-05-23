dcf6d78b6825460f532877e7d3dae7a70e7a0d27
SlimeNull <slimenull@outlook.com>
2024-05-23 09:03:24 +0800

    feat: 优化 TabItem 样式

---
+++ EleCho.WpfSuite/Converters/InvertThicknessConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace EleCho.WpfSuite
+{
+    public class InvertThicknessConverter : SingletonValueConverterBase<InvertThicknessConverter>
+    {
+        public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not Thickness thickness)
+                throw new ArgumentException("Invalid type of value", nameof(value));
+
+            return new Thickness(-thickness.Left, -thickness.Top, -thickness.Right, -thickness.Bottom);
+        }
+
+        public override object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not Thickness thickness)
+                throw new ArgumentException("Invalid type of value", nameof(value));
+
+            return new Thickness(-thickness.Left, -thickness.Top, -thickness.Right, -thickness.Bottom);
+        }
+    }
+}
