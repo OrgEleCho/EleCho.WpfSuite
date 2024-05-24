c44114ad55133285962ed4fd7261a434bc199641
SlimeNull <slimenull@outlook.com>
2024-05-24 11:24:20 +0800

    feat: 优化 ScrollViewer, CheckBox, 实现 ScrollBar, Thumb, RepeatButton

--- EleCho.WpfSuite/Controls/CheckBox.cs
+++ EleCho.WpfSuite/Controls/CheckBox.cs
@@ -5,19 +5,15 @@ namespace EleCho.WpfSuite
 {
     public class CheckBox : System.Windows.Controls.CheckBox
     {
-        private static readonly Brush s_glyphBrush;
-        private static readonly Brush s_disabledGlyphBrush;
-        private static readonly Geometry s_glyph;
+        private static readonly Brush s_glyphBrush = new SolidColorBrush(Color.FromRgb(33, 33, 33));
+        private static readonly Brush s_disabledGlyphBrush = new SolidColorBrush(Color.FromRgb(112, 112, 112));
+        private static readonly Geometry s_glyph = Geometry.Parse("F1 M 9.97498,1.22334L 4.6983,9.09834L 4.52164,9.09834L 0,5.19331L 1.27664,3.52165L 4.255,6.08833L 8.33331,1.52588e-005L 9.97498,1.22334 Z");
 
         static CheckBox()
         {
-            s_glyphBrush = new SolidColorBrush(Color.FromRgb(33, 33, 33));
             s_glyphBrush.Freeze();
-
-            s_disabledGlyphBrush = new SolidColorBrush(Color.FromRgb(112, 112, 112));
             s_disabledGlyphBrush.Freeze();
-
-            s_glyph = Geometry.Parse("F1 M 9.97498,1.22334L 4.6983,9.09834L 4.52164,9.09834L 0,5.19331L 1.27664,3.52165L 4.255,6.08833L 8.33331,1.52588e-005L 9.97498,1.22334 Z");
+            s_glyph.Freeze();
 
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBox), new FrameworkPropertyMetadata(typeof(CheckBox)));
         }
@@ -28,8 +24,6 @@ namespace EleCho.WpfSuite
             get { return (CornerRadius)GetValue(CornerRadiusProperty); }
             set { SetValue(CornerRadiusProperty, value); }
         }
-
-
 
 
         public Geometry? Glyph
---
+++ EleCho.WpfSuite/Controls/Thumb.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace EleCho.WpfSuite
+{
+    public class Thumb : System.Windows.Controls.Primitives.Thumb
+    {
+        static Thumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Thumb), new FrameworkPropertyMetadata(typeof(Thumb)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public Brush HoverBackground
+        {
+            get { return (Brush)GetValue(HoverBackgroundProperty); }
+            set { SetValue(HoverBackgroundProperty, value); }
+        }
+
+        public Brush PressedBackground
+        {
+            get { return (Brush)GetValue(PressedBackgroundProperty); }
+            set { SetValue(PressedBackgroundProperty, value); }
+        }
+
+        public Brush DisabledBackground
+        {
+            get { return (Brush)GetValue(DisabledBackgroundProperty); }
+            set { SetValue(DisabledBackgroundProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            Border.CornerRadiusProperty.AddOwner(typeof(Thumb));
+
+        public static readonly DependencyProperty HoverBackgroundProperty =
+            DependencyProperty.Register(nameof(HoverBackground), typeof(Brush), typeof(Thumb), new FrameworkPropertyMetadata(null));
+
+        public static readonly DependencyProperty PressedBackgroundProperty =
+            DependencyProperty.Register(nameof(PressedBackground), typeof(Brush), typeof(Thumb), new FrameworkPropertyMetadata(null));
+
+        public static readonly DependencyProperty DisabledBackgroundProperty =
+            DependencyProperty.Register(nameof(DisabledBackground), typeof(Brush), typeof(Thumb), new FrameworkPropertyMetadata(null));
+    }
+}
