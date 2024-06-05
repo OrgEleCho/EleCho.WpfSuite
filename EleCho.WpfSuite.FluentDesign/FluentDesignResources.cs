60aeadbeab359c554bd98685c0c4740e0bb5f29a
SlimeNull <slimenull@outlook.com>
2024-06-05 21:48:01 +0800

    feat: 加入演示用的 FluentDesign 样式

---
+++ EleCho.WpfSuite.FluentDesign/FluentDesignResources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace EleCho.WpfSuite.FluentDesign
+{
+    public class FluentDesignResources : ResourceDictionary
+    {
+        public FluentDesignResources()
+        {
+            MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("pack://application:,,,/EleCho.WpfSuite.FluentDesign;component/FluentDesignResources.xaml") });
+        }
+    }
+}
