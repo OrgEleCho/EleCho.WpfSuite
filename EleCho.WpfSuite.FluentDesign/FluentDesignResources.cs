f413b224a44b94a975aeb49ca8fc2aee52a29f74
SlimeNull <slimenull@outlook.com>
2024-06-06 18:28:31 +0800

    feat: 优化 FluentDesign 实现

--- EleCho.WpfSuite.FluentDesign/FluentDesignResources.cs
+++ EleCho.WpfSuite.FluentDesign/FluentDesignResources.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Reflection;
 using System.Windows;
 
 namespace EleCho.WpfSuite.FluentDesign
 {
+    public class FluentDesignResourceKey : ResourceKey
+    {
+        private static readonly Assembly s_thisAssembly = Assembly.GetExecutingAssembly();
+
+        public override Assembly Assembly => s_thisAssembly;
+    }
+
     public class FluentDesignResources : ResourceDictionary
     {
         public FluentDesignResources()
         {
             MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("pack://application:,,,/EleCho.WpfSuite.FluentDesign;component/FluentDesignResources.xaml") });
         }
+
+        
     }
 }
