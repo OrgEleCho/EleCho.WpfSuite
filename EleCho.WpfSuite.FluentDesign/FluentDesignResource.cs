89967aac3568c2e6ddadace6af536b842313c2e2
SlimeNull <slimenull@outlook.com>
2024-07-13 11:19:08 +0800

    feat: change fluent design resource key implementation

---
+++ EleCho.WpfSuite.FluentDesign/FluentDesignResource.cs
@@ -0,0 +1,25 @@
+﻿namespace EleCho.WpfSuite.FluentDesign
+{
+    public enum FluentDesignResource
+    {
+        TextColor,
+        PrimaryColor,
+        ControlBackgroundColor,
+        ControlHoverBackgroundColor,
+        ControlPressedBackgroundColor,
+        TextBoxBackgroundColor,
+        TextBoxHoverBackgroundColor,
+        TextBoxFocusedBackgroundColor,
+
+        TextBrush,
+        PrimaryBrush,
+        ControlBackgroundBrush,
+        ControlHoverBackgroundBrush,
+        ControlPressedBackgroundBrush,
+        TextBoxBackgroundBrush,
+        TextBoxHoverBackgroundBrush,
+        TextBoxFocusedBackgroundBrush,
+        ControlBorderBrush,
+        TextBoxBorderBrush,
+    }
+}
