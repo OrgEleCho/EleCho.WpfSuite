c2bbc356dbe8b0965beaeb3ba41e6d9c4e394f5c
SlimeNull <slimenull@outlook.com>
2024-08-22 16:42:06 +0800

    refactor: move 'UnsafeRect' to new file

---
+++ EleCho.WpfSuite/Panels/RelativePanel.Structures.cs
@@ -0,0 +1,22 @@
+﻿namespace EleCho.WpfSuite
+{
+
+    public partial class RelativePanel
+    {
+        private struct UnsafeRect
+        {
+            public double X;
+            public double Y;
+            public double Width;
+            public double Height;
+
+            public UnsafeRect(double x, double y, double width, double height)
+            {
+                X = x;
+                Y = y;
+                Width = width;
+                Height = height;
+            }
+        }
+    }
+}
