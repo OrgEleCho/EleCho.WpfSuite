3ef5d96712e99c2f35e3953c54ad57096e02a7b4
SlimeNull <slimenull@outlook.com>
2024-08-22 20:06:32 +0800

    test: 添加了单元测试项目

---
+++ UnitTests/CrossThreadTestRunner.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Security.Permissions;
+
+namespace UnitTests
+{
+    public class CrossThreadTestRunner
+    {
+        private Exception? lastException;
+
+        public void RunInMTA(ThreadStart userDelegate)
+        {
+            Run(userDelegate, ApartmentState.MTA);
+        }
+
+        public void RunInSTA(ThreadStart userDelegate)
+        {
+            Run(userDelegate, ApartmentState.STA);
+        }
+
+        private void Run(ThreadStart userDelegate, ApartmentState apartmentState)
+        {
+            lastException = null;
+
+            Thread thread = new Thread(
+              delegate()
+              {
+                  try
+                  {
+                      userDelegate.Invoke();
+                  }
+                  catch (Exception e)
+                  {
+                      lastException = e;
+                  }
+              });
+            thread.SetApartmentState(apartmentState);
+
+            thread.Start();
+            thread.Join();
+
+            if (lastException is not null)
+                throw lastException;
+        }
+    }
+}
---
+++ UnitTests/RelativePanelTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows;
+using System.Windows.Shapes;
+using EleCho.WpfSuite;
+
+namespace UnitTests
+{
+
+    public class RelativePanelTests
+    {
+        [Test]
+        [TestCase(100, 100)]
+        public void RelativePanelMeasuresCorrectly1(double availableWidth, double availableHeight)
+        {
+            CrossThreadTestRunner runner = new CrossThreadTestRunner();
+            runner.RunInSTA(() =>
+            {
+                var rect1 = new Rectangle { Height = 20, Width = 20 };
+                var rect2 = new Rectangle { Height = 20, Width = 20 };
+
+                var target = new RelativePanel
+                {
+                    Children =
+                    {
+                        rect1, rect2
+                    }
+                };
+
+                RelativePanel.SetBelow(rect2, rect1);
+                target.Measure(new Size(availableWidth, availableHeight));
+                target.Arrange(new Rect(0, 0, availableWidth, availableHeight));
+
+                Assert.AreEqual(target.DesiredSize.Width, 20.0);
+                Assert.AreEqual(target.DesiredSize.Height, 40.0);
+            });
+        }
+
+        [Test]
+        [TestCase(100, 100)]
+        public void RelativePanelMeasuresCorrectly2(double availableWidth, double availableHeight)
+        {
+            CrossThreadTestRunner runner = new CrossThreadTestRunner();
+            runner.RunInSTA(() =>
+            {
+                var rect1 = new Rectangle { Height = 20, Width = 20 };
+                var rect2 = new Rectangle { Height = 20, Width = 20 };
+
+                var target = new RelativePanel
+                {
+                    Children =
+                    {
+                        rect1, rect2
+                    }
+                };
+
+                RelativePanel.SetRightOf(rect2, rect1);
+                target.Measure(new Size(availableWidth, availableHeight));
+                target.Arrange(new Rect(0, 0, availableWidth, availableHeight));
+
+                Assert.AreEqual(target.DesiredSize.Width, 40.0);
+                Assert.AreEqual(target.DesiredSize.Height, 20.0);
+            });
+        }
+
+        [Test]
+        public void RelativePanelMeasuresCorrectly3()
+        {
+            CrossThreadTestRunner runner = new CrossThreadTestRunner();
+            runner.RunInSTA(() =>
+            {
+                var rect1 = new Rectangle { Height = 20, Width = 20 };
+                var rect2 = new Rectangle { Height = 20, Width = 20 };
+                var rect3 = new Rectangle { Height = 20, Width = 20 };
+
+                var target = new RelativePanel
+                {
+                    Children =
+                    {
+                        rect1, rect2, rect3
+                    }
+                };
+
+                RelativePanel.SetAlignHorizontalCenterWithPanel(rect1, true);
+                RelativePanel.SetRightOf(rect2, rect1);
+                RelativePanel.SetRightOf(rect3, rect2);
+
+                target.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                target.Arrange(new Rect(0, 0, target.DesiredSize.Width, target.DesiredSize.Height));
+
+                Assert.AreEqual(100.0, target.ActualWidth);
+                Assert.AreEqual(20, target.ActualHeight);
+            });
+        }
+
+    }
+}
