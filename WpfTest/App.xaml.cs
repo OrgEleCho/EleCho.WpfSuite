2c4aa04ada4073942fa50db970f5b7a102b23350
SlimeNull <slimenull@outlook.com>
2024-09-01 04:32:13 +0800

    feat: better sample app

--- WpfTest/App.xaml.cs
+++ WpfTest/App.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Xml;
+using EleCho.WpfSuite.FluentDesign;
 
 namespace WpfTest
 {
@@ -37,6 +38,13 @@ namespace WpfTest
                 
             }
 
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ApplicationThemeUtilities.SetApplicationTheme(ApplicationTheme.Auto);
+
+            base.OnStartup(e);
         }
     }
 
