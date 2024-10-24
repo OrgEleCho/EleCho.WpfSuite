b9666fcdb203e87d86edd2b4487e785f3af957b1
SlimeNull <slimenull@outlook.com>
2024-10-24 15:43:46 +0800

    fix: 修复命名空间导致的构建失败

--- EleCho.WpfSuite.Media/AssemblyInfo.cs
+++ EleCho.WpfSuite.Media/AssemblyInfo.cs
@@ -3,8 +3,6 @@ using System.Windows.Markup;
 
 [assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("https://schemas.elecho.dev/wpfsuite", "EleCho.WpfSuite")]
-[assembly: XmlnsDefinition("https://schemas.elecho.dev/wpfsuite", "EleCho.WpfSuite.Markup")]
-[assembly: XmlnsDefinition("https://schemas.elecho.dev/wpfsuite", "EleCho.WpfSuite.Controls")]
 [assembly: XmlnsDefinition("https://schemas.elecho.dev/wpfsuite", "EleCho.WpfSuite.Media.Animation")]
 [assembly: XmlnsDefinition("https://schemas.elecho.dev/wpfsuite", "EleCho.WpfSuite.Media.Transition")]
 
