3d7ca5cdce3eb85a70aedd4cb3ec5d34c3aa2f7a
SlimeNull <slimenull@outlook.com>
2024-11-28 01:33:23 +0800

    feat: 添加 InvertNumberConverter

---
+++ EleCho.WpfSuite.ValueConverters/ValueConverters/InvertNumberConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EleCho.WpfSuite.ValueConverters
+{
+    /// <summary>
+    /// Value converter that returns the reciprocal of number
+    /// </summary>
+    public class InvertNumberConverter : SingletonValueConverterBase<InvertNumberConverter, double, double>
+    {
+        /// <inheritdoc/>
+        public override double Convert(double value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return 1 / value;
+        }
+
+        /// <inheritdoc/>
+        public override double ConvertBack(double value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
---
+++ ProjectDependency.md
@@ -0,0 +1,42 @@
+Common was depended by:
+
+- Controls
+- Helpers
+- Layout
+- Media
+
+Controls was depended by:
+
+- ~~Nothing~~
+
+
+Helpers was depended by:
+
+- ~~Nothing~~
+
+
+Input was depended by:
+
+- ~~Nothing~~
+
+
+Layout was depended by:
+
+- ~~Nothing~~
+
+
+Markup was depended by:
+
+- ~~Nothing~~
+
+
+Media was depended by:
+
+- ~~Nothing~~
+
+
+ValueConverters was depended by:
+
+- Controls
+
+
