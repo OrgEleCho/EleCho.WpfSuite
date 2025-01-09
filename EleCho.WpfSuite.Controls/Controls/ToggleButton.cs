1ca2a8c2da53e18c5d761b0574f19499009cd0ec
SlimeNull <slimenull@outlook.com>
2025-01-09 21:46:22 +0800

    feat: 完成 ToggleButton

--- EleCho.WpfSuite.Controls/Controls/ToggleButton.cs
+++ EleCho.WpfSuite.Controls/Controls/ToggleButton.cs
@@ -12,146 +12,29 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EleCho.WpfSuite.Controls.SourceGeneration;
+using EleCho.WpfSuite.Controls.States;
 
 namespace EleCho.WpfSuite.Controls
 {
-    public class ToggleButton : System.Windows.Controls.Primitives.ToggleButton
+    [GenerateStates]
+    [GenerateStatesState(State.Hover)]
+    [GenerateStatesState(State.Pressed)]
+    [GenerateStatesState(State.Checked)]
+    [GenerateStatesState(State.Highlighted)]
+    [GenerateStatesState(State.Disabled)]
+    [GenerateStatesProperty(StateProperty.Background)]
+    [GenerateStatesProperty(StateProperty.Foreground)]
+    [GenerateStatesProperty(StateProperty.BorderBrush)]
+    [GenerateStatesProperty(StateProperty.Padding)]
+    [GenerateStatesProperty(StateProperty.BorderThickness)]
+    [GenerateStatesProperty(StateProperty.CornerRadius)]
+    [GenerateCornerRadiusProperty]
+    public partial class ToggleButton : System.Windows.Controls.Primitives.ToggleButton
     {
         static ToggleButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleButton), new FrameworkPropertyMetadata(typeof(ToggleButton)));
         }
-
-        /// <summary>
-        /// The CornerRadius property allows users to control the roundness of the corners independently by
-        /// setting a radius value for each corner.  Radius values that are too large are scaled so that they
-        /// smoothly blend from corner to corner.
-        /// </summary>
-        public CornerRadius CornerRadius
-        {
-            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
-            set { SetValue(CornerRadiusProperty, value); }
-        }
-
-        public Brush HoverForeground
-        {
-            get { return (Brush)GetValue(HoverForegroundProperty); }
-            set { SetValue(HoverForegroundProperty, value); }
-        }
-
-        public Brush HoverBackground
-        {
-            get { return (Brush)GetValue(HoverBackgroundProperty); }
-            set { SetValue(HoverBackgroundProperty, value); }
-        }
-
-        public Brush HoverBorderBrush
-        {
-            get { return (Brush)GetValue(HoverBorderBrushProperty); }
-            set { SetValue(HoverBorderBrushProperty, value); }
-        }
-
-        public Brush PressedForeground
-        {
-            get { return (Brush)GetValue(PressedForegroundProperty); }
-            set { SetValue(PressedForegroundProperty, value); }
-        }
-
-        public Brush PressedBackground
-        {
-            get { return (Brush)GetValue(PressedBackgroundProperty); }
-            set { SetValue(PressedBackgroundProperty, value); }
-        }
-
-        public Brush PressedBorderBrush
-        {
-            get { return (Brush)GetValue(PressedBorderBrushProperty); }
-            set { SetValue(PressedBorderBrushProperty, value); }
-        }
-
-        public Brush CheckedForeground
-        {
-            get { return (Brush)GetValue(CheckedForegroundProperty); }
-            set { SetValue(CheckedForegroundProperty, value); }
-        }
-
-        public Brush CheckedBackground
-        {
-            get { return (Brush)GetValue(CheckedBackgroundProperty); }
-            set { SetValue(CheckedBackgroundProperty, value); }
-        }
-
-        public Brush CheckedBorderBrush
-        {
-            get { return (Brush)GetValue(CheckedBorderBrushProperty); }
-            set { SetValue(CheckedBorderBrushProperty, value); }
-        }
-
-        public Brush DisabledForeground
-        {
-            get { return (Brush)GetValue(DisabledForegroundProperty); }
-            set { SetValue(DisabledForegroundProperty, value); }
-        }
-
-        public Brush DisabledBackground
-        {
-            get { return (Brush)GetValue(DisabledBackgroundProperty); }
-            set { SetValue(DisabledBackgroundProperty, value); }
-        }
-
-        public Brush DisabledBorderBrush
-        {
-            get { return (Brush)GetValue(DisabledBorderBrushProperty); }
-            set { SetValue(DisabledBorderBrushProperty, value); }
-        }
-
-        public Brush HighlightBrush
-        {
-            get { return (Brush)GetValue(HighlightBrushProperty); }
-            set { SetValue(HighlightBrushProperty, value); }
-        }
-
-
-        public static readonly DependencyProperty CornerRadiusProperty =
-            Border.CornerRadiusProperty.AddOwner(typeof(ToggleButton));
-
-        public static readonly DependencyProperty HoverForegroundProperty =
-            DependencyProperty.Register(nameof(HoverForeground), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty HoverBackgroundProperty =
-            DependencyProperty.Register(nameof(HoverBackground), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty HoverBorderBrushProperty =
-            DependencyProperty.Register(nameof(HoverBorderBrush), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty PressedForegroundProperty =
-            DependencyProperty.Register(nameof(PressedForeground), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty PressedBackgroundProperty =
-            DependencyProperty.Register(nameof(PressedBackground), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty PressedBorderBrushProperty =
-            DependencyProperty.Register(nameof(PressedBorderBrush), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty CheckedForegroundProperty =
-            DependencyProperty.Register(nameof(CheckedForeground), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty CheckedBackgroundProperty =
-            DependencyProperty.Register(nameof(CheckedBackground), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty CheckedBorderBrushProperty =
-            DependencyProperty.Register(nameof(CheckedBorderBrush), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty DisabledForegroundProperty =
-            DependencyProperty.Register(nameof(DisabledForeground), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty DisabledBackgroundProperty =
-            DependencyProperty.Register(nameof(DisabledBackground), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty DisabledBorderBrushProperty =
-            DependencyProperty.Register(nameof(DisabledBorderBrush), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty HighlightBrushProperty =
-            DependencyProperty.Register(nameof(HighlightBrush), typeof(Brush), typeof(ToggleButton), new FrameworkPropertyMetadata(SystemColors.HighlightBrush));
     }
 }
--- EleCho.WpfSuite.Controls/Controls/Tooltip.cs
+++ EleCho.WpfSuite.Controls/Controls/Tooltip.cs
@@ -4,29 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using EleCho.WpfSuite.Controls.SourceGeneration;
 
 namespace EleCho.WpfSuite.Controls
 {
-    public class Tooltip : System.Windows.Controls.ToolTip
+    [GenerateCornerRadiusProperty]
+    public partial class Tooltip : System.Windows.Controls.ToolTip
     {
         static Tooltip()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Tooltip), new FrameworkPropertyMetadata(typeof(Tooltip)));
         }
-
-        /// <summary>
-        /// The CornerRadius property allows users to control the roundness of the corners independently by
-        /// setting a radius value for each corner.  Radius values that are too large are scaled so that they
-        /// smoothly blend from corner to corner.
-        /// </summary>
-        public CornerRadius CornerRadius
-        {
-            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
-            set { SetValue(CornerRadiusProperty, value); }
-        }
-
-
-        public static readonly DependencyProperty CornerRadiusProperty =
-            Border.CornerRadiusProperty.AddOwner(typeof(Tooltip));
     }
 }
