66cc1cdc6019e99bdc71dfc803eb38203633f16d
SlimeNull <slimenull@outlook.com>
2025-01-19 18:11:54 +0800

    feat: ScrollBar 做好哩!

--- EleCho.WpfSuite.Controls/Controls/ScrollBar.cs
+++ EleCho.WpfSuite.Controls/Controls/ScrollBar.cs
@@ -1,9 +1,36 @@
 ﻿using System.Windows;
 using System.Windows.Media;
+using EleCho.WpfSuite.Controls.SourceGeneration;
+using EleCho.WpfSuite.Controls.States;
 
 namespace EleCho.WpfSuite.Controls
 {
-    public class ScrollBar : System.Windows.Controls.Primitives.ScrollBar
+    [GenerateStates]
+    [GenerateStatesState(State.Hover)]
+    [GenerateStatesState(State.Disabled)]
+    [GenerateStatesProperty(StateProperty.Background)]
+    [GenerateStatesProperty(StateProperty.BorderBrush)]
+    [GenerateStatesProperty(StateProperty.BorderThickness)]
+    [GenerateStatesProperty(StateProperty.CornerRadius)]
+    [GenerateComponentStatesState("Thumb", State.Hover)]
+    [GenerateComponentStatesState("Thumb", State.Dragging)]
+    [GenerateComponentStatesState("Thumb", State.Disabled)]
+    [GenerateComponentStateProperty("Thumb", StateProperty.Background)]
+    [GenerateComponentStateProperty("Thumb", StateProperty.BorderBrush)]
+    [GenerateComponentStateProperty("Thumb", StateProperty.BorderThickness)]
+    [GenerateComponentStateProperty("Thumb", StateProperty.CornerRadius)]
+    [GenerateComponentStatesState("Button", State.Highlighted)]
+    [GenerateComponentStatesState("Button", State.Hover)]
+    [GenerateComponentStatesState("Button", State.Pressed)]
+    [GenerateComponentStatesState("Button", State.Disabled)]
+    [GenerateComponentStateProperty("Button", StateProperty.Background)]
+    [GenerateComponentStateProperty("Button", StateProperty.GlyphBrush)]
+    [GenerateComponentStateProperty("Button", StateProperty.BorderBrush)]
+    [GenerateComponentStateProperty("Button", StateProperty.Padding)]
+    [GenerateComponentStateProperty("Button", StateProperty.BorderThickness)]
+    [GenerateComponentStateProperty("Button", StateProperty.CornerRadius)]
+    [GenerateCornerRadiusProperty]
+    public partial class ScrollBar : System.Windows.Controls.Primitives.ScrollBar
     {
         private static readonly Brush s_thumbBrush =new SolidColorBrush(Color.FromRgb(205, 205, 205));
         private static readonly Brush s_glyphBrush= new SolidColorBrush(Color.FromRgb(96, 96, 96));
@@ -26,33 +53,6 @@ namespace EleCho.WpfSuite.Controls
 
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ScrollBar), new FrameworkPropertyMetadata(typeof(ScrollBar)));
         }
-
-
-        public CornerRadius CornerRadius
-        {
-            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
-            set { SetValue(CornerRadiusProperty, value); }
-        }
-
-        public CornerRadius ThumbCornerRadius
-        {
-            get { return (CornerRadius)GetValue(ThumbCornerRadiusProperty); }
-            set { SetValue(ThumbCornerRadiusProperty, value); }
-        }
-
-        public CornerRadius ButtonCornerRadius
-        {
-            get { return (CornerRadius)GetValue(ButtonCornerRadiusProperty); }
-            set { SetValue(ButtonCornerRadiusProperty, value); }
-        }
-
-        public Thickness GlyphMargin
-        {
-            get { return (Thickness)GetValue(GlyphMarginProperty); }
-            set { SetValue(GlyphMarginProperty, value); }
-        }
-
-
 
         public Geometry? ArrowLeftGlyph
         {
@@ -78,88 +78,10 @@ namespace EleCho.WpfSuite.Controls
             set { SetValue(ArrowDownGlyphProperty, value); }
         }
 
-        public Brush ThumbBrush
+        public double ButtonSize
         {
-            get { return (Brush)GetValue(ThumbBrushProperty); }
-            set { SetValue(ThumbBrushProperty, value); }
-        }
-
-        public Brush GlyphBrush
-        {
-            get { return (Brush)GetValue(GlyphBrushProperty); }
-            set { SetValue(GlyphBrushProperty, value); }
-        }
-
-        public Brush HoverBackground
-        {
-            get { return (Brush)GetValue(HoverBackgroundProperty); }
-            set { SetValue(HoverBackgroundProperty, value); }
-        }
-
-        public Brush HoverBorderBrush
-        {
-            get { return (Brush)GetValue(HoverBorderBrushProperty); }
-            set { SetValue(HoverBorderBrushProperty, value); }
-        }
-
-        public Brush HoverThumbBrush
-        {
-            get { return (Brush)GetValue(HoverThumbBrushProperty); }
-            set { SetValue(HoverThumbBrushProperty, value); }
-        }
-
-        public Brush HoverGlyphBrush
-        {
-            get { return (Brush)GetValue(HoverGlyphBrushProperty); }
-            set { SetValue(HoverGlyphBrushProperty, value); }
-        }
-
-        public Brush PressedBackground
-        {
-            get { return (Brush)GetValue(PressedBackgroundProperty); }
-            set { SetValue(PressedBackgroundProperty, value); }
-        }
-
-        public Brush PressedBorderBrush
-        {
-            get { return (Brush)GetValue(PressedBorderBrushProperty); }
-            set { SetValue(PressedBorderBrushProperty, value); }
-        }
-
-        public Brush DraggingThumbBrush
-        {
-            get { return (Brush)GetValue(DraggingThumbBrushProperty); }
-            set { SetValue(DraggingThumbBrushProperty, value); }
-        }
-
-        public Brush PressedGlyphBrush
-        {
-            get { return (Brush)GetValue(PressedGlyphBrushProperty); }
-            set { SetValue(PressedGlyphBrushProperty, value); }
-        }
-
-        public Brush DisabledBackground
-        {
-            get { return (Brush)GetValue(DisabledBackgroundProperty); }
-            set { SetValue(DisabledBackgroundProperty, value); }
-        }
-
-        public Brush DisabledBorderBrush
-        {
-            get { return (Brush)GetValue(DisabledBorderBrushProperty); }
-            set { SetValue(DisabledBorderBrushProperty, value); }
-        }
-
-        public Brush DisabledThumbBrush
-        {
-            get { return (Brush)GetValue(DisabledThumbBrushProperty); }
-            set { SetValue(DisabledThumbBrushProperty, value); }
-        }
-
-        public Brush DisabledGlyphBrush
-        {
-            get { return (Brush)GetValue(DisabledGlyphBrushProperty); }
-            set { SetValue(DisabledGlyphBrushProperty, value); }
+            get { return (double)GetValue(ButtonSizeProperty); }
+            set { SetValue(ButtonSizeProperty, value); }
         }
 
         public bool IsButtonVisible
@@ -171,18 +93,6 @@ namespace EleCho.WpfSuite.Controls
 
 
 
-
-        public static readonly DependencyProperty CornerRadiusProperty =
-            Border.CornerRadiusProperty.AddOwner(typeof(ScrollBar));
-
-        public static readonly DependencyProperty ThumbCornerRadiusProperty =
-            DependencyProperty.Register(nameof(ThumbCornerRadius), typeof(CornerRadius), typeof(ScrollBar), new FrameworkPropertyMetadata(new CornerRadius(0)));
-
-        public static readonly DependencyProperty ButtonCornerRadiusProperty =
-            DependencyProperty.Register(nameof(ButtonCornerRadius), typeof(CornerRadius), typeof(ScrollBar), new FrameworkPropertyMetadata(new CornerRadius(0)));
-
-        public static readonly DependencyProperty GlyphMarginProperty =
-            DependencyProperty.Register(nameof(GlyphMargin), typeof(Thickness), typeof(ScrollBar), new FrameworkPropertyMetadata(new Thickness(3)));
 
         public static readonly DependencyProperty ArrowLeftGlyphProperty =
             DependencyProperty.Register(nameof(ArrowLeftGlyph), typeof(Geometry), typeof(ScrollBar), new FrameworkPropertyMetadata(s_glyphLeft));
@@ -196,47 +106,8 @@ namespace EleCho.WpfSuite.Controls
         public static readonly DependencyProperty ArrowDownGlyphProperty =
             DependencyProperty.Register(nameof(ArrowDownGlyph), typeof(Geometry), typeof(ScrollBar), new FrameworkPropertyMetadata(s_glyphDown));
 
-        public static readonly DependencyProperty ThumbBrushProperty =
-            DependencyProperty.Register(nameof(ThumbBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(s_thumbBrush));
-
-        public static readonly DependencyProperty GlyphBrushProperty =
-            DependencyProperty.Register(nameof(GlyphBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(s_glyphBrush));
-
-        public static readonly DependencyProperty HoverBackgroundProperty =
-            DependencyProperty.Register(nameof(HoverBackground), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty HoverBorderBrushProperty =
-            DependencyProperty.Register(nameof(HoverBorderBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty HoverThumbBrushProperty =
-            DependencyProperty.Register(nameof(HoverThumbBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty HoverGlyphBrushProperty =
-            DependencyProperty.Register(nameof(HoverGlyphBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty PressedBackgroundProperty =
-            DependencyProperty.Register(nameof(PressedBackground), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty PressedBorderBrushProperty =
-            DependencyProperty.Register(nameof(PressedBorderBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty DraggingThumbBrushProperty =
-            DependencyProperty.Register(nameof(DraggingThumbBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty PressedGlyphBrushProperty =
-            DependencyProperty.Register(nameof(PressedGlyphBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty DisabledBackgroundProperty =
-            DependencyProperty.Register(nameof(DisabledBackground), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty DisabledBorderBrushProperty =
-            DependencyProperty.Register(nameof(DisabledBorderBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty DisabledThumbBrushProperty =
-            DependencyProperty.Register(nameof(DisabledThumbBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty DisabledGlyphBrushProperty =
-            DependencyProperty.Register(nameof(DisabledGlyphBrush), typeof(Brush), typeof(ScrollBar), new FrameworkPropertyMetadata(s_disabledGlyphBrush));
+        public static readonly DependencyProperty ButtonSizeProperty =
+            DependencyProperty.Register(nameof(ButtonSize), typeof(double), typeof(ScrollBar), new PropertyMetadata(10.0));
 
         public static readonly DependencyProperty IsButtonVisibleProperty =
             DependencyProperty.Register(nameof(IsButtonVisible), typeof(bool), typeof(ScrollBar), new FrameworkPropertyMetadata(true));
