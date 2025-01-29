25c9984861a69c8eaeabba2adfca15001646772c
SlimeNull <slimenull@outlook.com>
2025-01-30 00:20:16 +0800

    feat: bug test proj

---
+++ BugTest22/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BugTest22
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
