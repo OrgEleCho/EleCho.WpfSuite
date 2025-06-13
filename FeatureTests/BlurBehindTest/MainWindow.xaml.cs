4bdd53f5f251774d35f7aad4094afc64a748e387
SlimeNull <slimenull@outlook.com>
2025-06-13 16:12:06 +0800

    feat: 更新测试项

--- FeatureTests/BlurBehindTest/MainWindow.xaml.cs
+++ FeatureTests/BlurBehindTest/MainWindow.xaml.cs
@@ -20,4 +20,50 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private Point _startElementPosition;
+    private Point _startMousePosition;
+
+    private void SiblingPresenter_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is not UIElement blurArea)
+        {
+            return;
+        }
+
+        blurArea.CaptureMouse();
+
+        if (blurArea.IsMouseCaptured)
+        {
+            _startElementPosition = new Point(Canvas.GetLeft(blurArea), Canvas.GetTop(blurArea));
+            _startMousePosition = Mouse.GetPosition(null);
+        }
+    }
+
+    private void SiblingPresenter_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (sender is not UIElement blurArea)
+        {
+            return;
+        }
+
+        if (blurArea.IsMouseCaptured)
+        {
+            var nowPosition = Mouse.GetPosition(null);
+            var offset = nowPosition - _startMousePosition;
+
+            Canvas.SetLeft(blurArea, _startElementPosition.X + offset.X);
+            Canvas.SetTop(blurArea, _startElementPosition.Y + offset.Y);
+        }
+    }
+
+    private void SiblingPresenter_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is not UIElement blurArea)
+        {
+            return;
+        }
+
+        blurArea.ReleaseMouseCapture();
+    }
 }
