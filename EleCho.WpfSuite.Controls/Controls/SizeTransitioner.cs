950a7eea87af748850b31e36856748c7009f906e
SlimeNull <slimenull@outlook.com>
2026-06-05 15:59:01 +0800

    feat: 支持 SizeTransitioner 和 VisibilityTransitioner

---
+++ EleCho.WpfSuite.Controls/Controls/SizeTransitioner.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace EleCho.WpfSuite.Controls
+{
+    public class SizeTransitioner : Decorator
+    {
+        private Size _targetMeasureSize;
+        private static readonly IEasingFunction _easing;
+
+        static SizeTransitioner()
+        {
+            var easing = new SineEase() { EasingMode = EasingMode.EaseOut };
+            easing.Freeze();
+            _easing = easing;
+        }
+
+        private Size MeasureSizeAnimated
+        {
+            get { return (Size)GetValue(MeasureSizeAnimatedProperty); }
+            set { SetValue(MeasureSizeAnimatedProperty, value); }
+        }
+
+        public TimeSpan Duration
+        {
+            get { return (TimeSpan)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+
+        public IEasingFunction Easinng
+        {
+            get { return (IEasingFunction)GetValue(EasinngProperty); }
+            set { SetValue(EasinngProperty, value); }
+        }
+
+
+        private static readonly DependencyProperty MeasureSizeAnimatedProperty =
+            DependencyProperty.Register(nameof(MeasureSizeAnimated), typeof(Size), typeof(SizeTransitioner),
+                new FrameworkPropertyMetadata(default(Size), FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public static readonly DependencyProperty DurationProperty =
+            DependencyProperty.Register(nameof(Duration), typeof(TimeSpan), typeof(SizeTransitioner),
+                new FrameworkPropertyMetadata(TimeSpan.FromSeconds(0.15), FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public static readonly DependencyProperty EasinngProperty =
+            DependencyProperty.Register(nameof(Easinng), typeof(IEasingFunction), typeof(SizeTransitioner),
+                new FrameworkPropertyMetadata(_easing, FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        protected virtual Size GetTargetMeasureSize(Size constraint)
+        {
+            return base.MeasureOverride(constraint);
+        }
+
+        protected virtual void PrepareTransition(ref Size initialSize, Size targetSize)
+        {
+
+        }
+
+        protected sealed override Size MeasureOverride(Size constraint)
+        {
+            var measureSizeAnimated = MeasureSizeAnimated;
+            var currentMeasureSize = GetTargetMeasureSize(constraint);
+
+            if (ReadLocalValue(MeasureSizeAnimatedProperty) == DependencyProperty.UnsetValue)
+            {
+                _targetMeasureSize = currentMeasureSize;
+                MeasureSizeAnimated = currentMeasureSize;
+                return currentMeasureSize;
+            }
+
+            if (currentMeasureSize != _targetMeasureSize)
+            {
+                var initialSize = DesiredSize;
+                var targetSizze = currentMeasureSize;
+
+                PrepareTransition(ref initialSize, targetSizze);
+
+                BeginAnimation(MeasureSizeAnimatedProperty, null);
+                BeginAnimation(MeasureSizeAnimatedProperty, new SizeAnimation
+                {
+                    From = initialSize,
+                    To = currentMeasureSize,
+                    Duration = Duration,
+                    FillBehavior = FillBehavior.HoldEnd,
+                    EasingFunction = _easing
+                });
+
+                _targetMeasureSize = currentMeasureSize;
+                return DesiredSize;
+            }
+
+            return measureSizeAnimated;
+        }
+
+        protected sealed override Size ArrangeOverride(Size arrangeBounds)
+        {
+            if (VisualChildrenCount > 0 &&
+                GetVisualChild(0) is UIElement elementChild)
+            {
+                var childRect = new Rect(0, 0, arrangeBounds.Width, arrangeBounds.Height);
+                elementChild.Arrange(childRect);
+            }
+
+            return arrangeBounds;
+        }
+    }
+}
---
+++ EleCho.WpfSuite.Controls/Controls/VisibilityTransitioner.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
+
+namespace EleCho.WpfSuite.Controls
+{
+    public class VisibilityTransitioner : SizeTransitioner
+    {
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(VisibilityTransitioner),
+                new FrameworkPropertyMetadata(Orientation.Vertical, FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register(nameof(IsOpen), typeof(bool), typeof(VisibilityTransitioner),
+                new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        protected override Size GetTargetMeasureSize(Size constraint)
+        {
+            var originSize = base.GetTargetMeasureSize(constraint);
+            if (IsOpen)
+            {
+                return originSize;
+            }
+
+            if (Orientation == Orientation.Vertical)
+            {
+                return new Size(originSize.Width, 0);
+            }
+            else
+            {
+                return new Size(0, originSize.Height);
+            }
+        }
+
+        protected override void PrepareTransition(ref Size initialSize, Size targetSize)
+        {
+            if (Orientation == Orientation.Vertical)
+            {
+                initialSize.Width = targetSize.Width;
+            }
+            else
+            {
+                initialSize.Height = targetSize.Height;
+            }
+        }
+    }
+}
